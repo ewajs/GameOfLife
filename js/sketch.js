10ca9eb8bf1eeeb50ed360f09c2df2584c665dee
ewajs <ezequiel.wajs@gmail.com>
2017-12-18 15:46:44 -0300

    Started working on controls

--- js/sketch.js
+++ js/sketch.js
@@ -10,11 +10,16 @@ var grid;
 
 
 function setup() {
+    // Canvas setup
     var canvas = createCanvas(WIDTH, HEIGHT);
     canvas.parent('canvas');
+
+    // Grid setup
     xitems = floor(WIDTH/resolution);
     yitems = floor(HEIGHT/resolution);
     grid = createGrid(xitems, yitems);
+
+    // Interface setup - Buttons
     var random = document.getElementById('random');
     random.addEventListener('click', randomGrid);
     var play = document.getElementById('play');
@@ -23,8 +28,33 @@ function setup() {
     pause.addEventListener('click', noLoop);
     var clean = document.getElementById('clean');
     clean.addEventListener('click', cleanGrid);
-    //console.log(grid);
-    //fillRandomly(grid);
+    var configure = document.getElementById('configure');
+    configure.addEventListener('click', function () {
+        resolution = document.getElementById('resolution').value;
+        xitems = floor(WIDTH/resolution);
+        yitems = floor(HEIGHT/resolution);
+        grid = createGrid(xitems, yitems);
+        drawGrid();
+    });
+
+    // Interface setup - Sliders
+    var overSlider = document.getElementById('overSlider');
+    var overValue = document.getElementById('overValue');
+    overSlider.value = overpopulation;
+    overValue.value = overpopulation;
+    bindSliderAndInput(overSlider, overValue, 'over');
+    var subSlider = document.getElementById('subSlider');
+    var subValue = document.getElementById('subValue');
+    subSlider.value = underpopulation;
+    subValue.value = underpopulation;
+    bindSliderAndInput(subSlider, subValue, 'under');
+    var birthSlider = document.getElementById('birthSlider');
+    var birthValue = document.getElementById('birthValue');
+    birthSlider.value = revive;
+    birthValue.value = revive;
+    bindSliderAndInput(birthSlider, birthValue, 'birth');
+
+
     setTimeout(mouseCheck, 1);
     frameRate(1);
     noLoop();
@@ -65,6 +95,7 @@ function cleanGrid() {
 }
 
 function drawGrid() {
+    clear();
     for (var i = 0; i < xitems; i++) {
         for(var j = 0; j < yitems; j++) {
             if(grid[i][j] == 0) {
@@ -72,7 +103,12 @@ function drawGrid() {
             } else {
                 fill(0);
             }
-            rect(i*resolution, j*resolution, resolution, resolution);
+            if (i == xitems - 1)
+                rect(i*resolution, j*resolution, resolution-1, resolution);
+            else if (j == yitems - 1)
+                rect(i*resolution, j*resolution, resolution, resolution-1);
+            else
+                rect(i*resolution, j*resolution, resolution, resolution);
         }
     }
 }
@@ -123,13 +159,40 @@ function mouseCheck() {
     if(mouseIsPressed) {
         var i = floor(mouseX/resolution);
         var j = floor(mouseY/resolution);
-        grid[i][j] = 1;
-        if(grid[i][j] == 0) {
-            fill(255);
-        } else {
-            fill(0);
+        if (i > 0 && j > 0 && i < xitems && j < yitems) {
+            grid[i][j] = 1;
+            if(grid[i][j] == 0) {
+                fill(255);
+            } else {
+                fill(0);
+            }
+            rect(i*resolution, j*resolution, resolution, resolution);
         }
-        rect(i*resolution, j*resolution, resolution, resolution);
     }
     setTimeout(mouseCheck, 1);
+}
+
+function bindSliderAndInput(slider, input, parameter) {
+    slider.addEventListener('change', function (){
+        input.value = slider.value;
+        switch (parameter) {
+            case 'over':
+                overpopulation = slider.value;
+                break;
+            case 'under':
+                underpopulation = slider.value;
+                break;
+            case 'birth':
+                revive = slider.value;
+                break;
+        }
+    });
+    input.addEventListener('change', function (){
+        if (input.value < 0) {
+            input.value = 0;
+        } else if (input.value > 8) {
+            input.value = 8;
+        }
+        slider.value = input.value;
+    });
 }
